f1b7550816bcfd2856164972e2fde6d26ce5831c
Dahai Li <dil@cisco.com>
2014-12-09 14:04:33 -0800

    Implemented the gathering of interface and ACLs for a given network

--- src/main/java/com/moonlite/discover/ASA.java
+++ src/main/java/com/moonlite/discover/ASA.java
@@ -1,14 +1,11 @@
 package com.moonlite.discover;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.*;
+import java.util.function.Function;
 
-import com.sun.xml.internal.ws.util.StringUtils;
-
-import javafx.util.Pair;
+import org.apache.commons.net.util.SubnetUtils;
+import org.apache.commons.net.util.SubnetUtils.SubnetInfo;
 
 /**
  * Data model of an ASA device.
@@ -30,10 +27,11 @@ public class ASA extends Host {
     /**
      * Gather the configuration information of the device.
      * @param network String, the filer used to identify the network of interest.
+     *        it must be in CDIR notation, e.g) 1.1.1.0/24.
      * @return configuration for the given network.
      * @throws IOException 
      */
-    public Configuration probe(String network) throws IOException {
+    public Configuration getConfiguration(String network) throws IOException {
         Configuration config = new Configuration();
         String path = "https://" +address + "/admin/config";
         String cfg  = new ApacheHttpClient().get(path, user, password);
@@ -44,12 +42,116 @@ public class ASA extends Host {
                 toArray(String[]::new);
         List<Object> commands = CompositeCommand.lines2Commands(lines, 0);
 
-        Stream<Object> accessLists = commands.stream().filter(cmd -> cmd.toString().startsWith("interface "));
-        config.accessLists = accessLists.toArray(Object[]::new);
+        //locate the interface that is within the network.
+        InterfaceCommand intf = getInterfaceCommand(commands, network);
+        if (intf == null) {
+            return null;
+        }
+        String nameIf = intf.getNameIf();
+        config.vlan = intf.getVLAN();
+        config.firewall = getFirewall(commands, nameIf);//NAT, and ServicePolicy?
+//        config.qos = getQoS(commands, nameIf);
 
         return config;
     }
 
+    /**
+     * Get the firewall related configurations, such as ACL, NAT, and ServicePolicy
+     * @param commands List<Object> String's and CompositeCommand's
+     * @param nameIf Sting, nameif from interface command.
+     * @return Object[] commands
+     */
+    private Object[] getFirewall(List<Object> commands, String nameIf) {
+        // get the access-group for the interface: "access-group inside_access_out out interface inside"
+        Set<String> aclNames = new HashSet<>();
+        commands.stream().
+                filter(cmd -> cmd.toString().startsWith("access-group ") &&
+                        (cmd.toString().endsWith(" global") ||  cmd.toString().split(" ")[4].equals(nameIf))).
+                forEach(cmd -> aclNames.add(cmd.toString().split(" ")[1]));
+        Object[] acls = commands.stream().
+                filter(cmd -> cmd.toString().startsWith("access-list ") && 
+                        aclNames.contains(cmd.toString().split(" ")[1])).
+                toArray();
+        //TODO take care of NAT, ServicePolicy?
+        return acls;
+    }
+
+    /**
+     * Get the InterfaceCommand for the interface in a given netowrk.
+     * @param commands List<Object> of ASA commands
+     * @param network String network
+     * @return InterfaceCommand for the interface that is in the given network
+     */
+    private static InterfaceCommand getInterfaceCommand(List<Object> commands, String network) {
+        Function <Object, Boolean> isInSubnet = obj -> {
+            if (obj instanceof String) {
+                return false;
+            }
+            CompositeCommand c = (CompositeCommand) obj;
+            if (!(c.command.startsWith("interface ")))
+                    return false;
+            SubnetInfo subnetInfo = new SubnetUtils(network).getInfo();
+            InterfaceCommand intf = new InterfaceCommand(c.command, c.subCommands);
+            String address = intf.getIpAddress();
+            return address != null && subnetInfo.isInRange(address);
+        };
+
+        Optional<Object> hit = commands.stream().
+                filter(c -> isInSubnet.apply(c)).
+                findFirst();
+        if (hit.isPresent()) {
+            CompositeCommand c = (CompositeCommand) hit.get();
+            return new InterfaceCommand(c.command, c.subCommands);
+        }
+        return null;
+    }
+
+    static class InterfaceCommand extends CompositeCommand {
+        InterfaceCommand(String command, List<Object> subCommands) {
+            super(command, subCommands);
+        }
+     
+        String getNameIf () {
+            String[] words = getSubCommandWords("nameif ");
+            if (words == null) {
+                return null;
+            } else {
+                return words[1];
+            }
+        }
+
+        String getIpAddress() {
+            String[] words = getSubCommandWords("ip address ");
+            if (words == null) {
+                return null;
+            }
+            return words[2];
+        }
+        
+        int getVLAN() {
+            String[] subCommandWords = getSubCommandWords("vlan ");
+            if (subCommandWords == null) {
+                return 0;
+            } else {
+                return Integer.parseInt(subCommandWords[1]);
+            }
+        }
+
+        /**
+         * Get the sub-command with the given prefix
+         * @param prefix String
+         * @return String[] words in the sub-command if found, or null
+         */
+        String[] getSubCommandWords(String prefix) {
+            Optional<Object> hit = this.subCommands.stream().filter(line -> ((String)line).startsWith(prefix)).findFirst();
+            if (hit.isPresent()) {
+                return ((String) hit.get()).split(" ");
+            } else {
+                return null;
+            }
+        }
+    }
+    
     /**
      * 
      * CompoisteCommand models ASA CLI that has sub-commands.
@@ -216,7 +318,9 @@ public class ASA extends Host {
     }
     
     static public class Configuration {
-        public Object[] accessLists;
+        public int vlan;
+        public Object[] buildingBlocks;
+        public Object[] firewall;
         public Object[] qos;
         private Object[] vpn;
     }
--- src/test/java/com/moonlite/discover/ASATest.java
+++ src/test/java/com/moonlite/discover/ASATest.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
-
 import org.junit.Test;
 
 import com.moonlite.discover.ASA.CompositeCommand;
@@ -15,18 +13,18 @@ import com.moonlite.discover.ASA.CompositeCommand;
 public class ASATest {
 
     @Test
-    public void test() {
-        ASA asa = new ASA("asav", "asadp", "dat@package");
+    public void testGetConfiguration() {
+        ASA asa = new ASA("my-f1", "cisco", "cisco");
         try {
-            ASA.Configuration config = asa.probe(null);
-            Arrays.stream(config.accessLists).forEach(System.out::println);
+            ASA.Configuration config = asa.getConfiguration("192.168.103.1/24");
+            Arrays.stream(config.firewall).forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
-            fail("ASA.probe");
+            fail("ASA.getConfiguration");
         }
     }
 
-    @Ignore
+    @Test
     public void testCompositeCommand() {
         String[] lines = {"main", " sub1", "  sub1.1", " sub2", "main2"};
         List<Object> commands = CompositeCommand.lines2Commands(lines, 0);
